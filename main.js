ce847f106e8c4419dd177ab67bd9791c742465e2
ecln30 <ecln30@gmail.com>
2022-04-28 10:27:24 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+// Your users should be able to:
+
+// View the optimal layout for the site depending on their device's screen size
+// See hover states for all interactive elements on the page
+// Switch between viewing Daily, Weekly, and Monthly stats
+
+
+
+
+
+
+
+async function getData() {
+     const res = await fetch("data.json")
+     const data = await res.json()
+
+     console.log(data)
+}
+
+
+getData()
+
+
+
+
+
+
+
+
+
+
+
+
